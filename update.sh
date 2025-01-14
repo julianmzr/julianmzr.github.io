3ed4a4bece655f25cc7e1390a9f200b43dcde673
julian-muenzer-snkeos <julian.muenzer@snkeos.com>
2025-01-14 18:20:30 +0100

    new queens features and update script

--- update.sh
+++ update.sh
@@ -1,1 +1,31 @@
-cp -rf modules/queens/build/web/* ./projects/queens
+#!/bin/bash
+
+build_flutter_project() {
+    local project_name=$1
+    echo "Building ${project_name} project..."
+    cd modules/${project_name}
+    flutter build web --base-href /projects/${project_name}/
+    cd ../..
+    rm -rf ./projects/${project_name}
+    mkdir -p ./projects/${project_name}
+    cp -rf modules/${project_name}/build/web/* ./projects/${project_name}
+}
+
+if [ $# -eq 0 ]; then
+    echo "Please provide an argument: 'queens' or 'all'"
+    exit 1
+fi
+
+case $1 in
+    "queens")
+        build_flutter_project "queens"
+        ;;
+    "all")
+        build_flutter_project "queens"
+        # Add other project builds here
+        ;;
+    *)
+        echo "Invalid argument. Please use 'queens' or 'all'"
+        exit 1
+        ;;
+esac
